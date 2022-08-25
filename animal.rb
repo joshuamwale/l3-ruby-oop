27d0347bf963a788b8908a123f3ee7e018640353
Joshua <mwalejoshua89@gmail.com>
2022-08-25 15:45:34 +0300

    Revise this code to master better

--- animal.rb
+++ animal.rb
@@ -1,3 +1,46 @@
 # TODO: CREATE CLASS AND INSTANCE METHODS (INCLUDE VARIABLES)
+class Animal
+
+    attr_reader :species
+    attr_writer :species
+
+    attr_accessor :life_expectancy
+
+    
+# getter for the name of the Animal
+    def name
+        @name
+    end
+
+    # setter for the name of the Animal
+    def name=(name)
+        @name = name
+    end
+
+
+
+   def sound
+    puts "#{self.name} is sounding"
+   end
+end
+
+dog = Animal.new
+dog.name = "Simba"
+dog.sound
+dog.species = "Chihuahua"
+dog.life_expectancy = 13
+
+
+cat = Animal.new
+cat.name = "Another Cat"
+puts cat
+cat.sound
+
+lioness = Animal.new
+
+lioness.name = "Nala"
+puts lioness.name
+lioness.sound
+
 
 # TODO: Use self to access instance variables
--- plant.rb
+++ plant.rb
@@ -1,1 +1,46 @@
 # TODO: Implement a class do illustrate how class variables and methods work.
+#items you expect to be common throughout
+
+class Plant
+
+    @@num_of_trees = 0
+    @@all_instances = []
+
+    attr_accessor :species
+
+    def initialize(species)
+        @species = species
+        @@num_of_trees += 1
+        info = {
+            instance_reference: self,
+            number: @@num_of_trees
+        }
+        @@all_instances << info
+end
+
+def speak_of_species
+    puts "#{self.species} is speaking"
+end
+
+def self.tree_count
+    @@num_of_trees
+end
+
+def self.show_instances
+    @@all_instances
+end
+
+end
+
+#ACCESS CLASS VARIABLE
+mwarubaini = Plant.new("Neem Tree")
+mwarubaini.speak_of_species
+puts Plant.tree_count
+
+eucalyptus = Plant.new("Eucalyptus Tree")
+mango = Plant.new("Mango Tree")
+ciala = Plant.new("Ciala Tree")
+cypress = Plant.new("Cypress")
+
+puts Plant.tree_count
+puts Plant.show_instances
